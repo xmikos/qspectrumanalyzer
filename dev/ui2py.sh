04da33e00a87d6273320dde9d413667064cfdd34
konung-yaropolk <43002351+konung-yaropolk@users.noreply.github.com>
2022-11-09 07:19:11 +0200

    Update ui2py.sh

--- dev/ui2py.sh
+++ dev/ui2py.sh
@@ -1,5 +1,11 @@
  #!/bin/bash
 
-pyuic5 abfplot_gui.ui  -o abfplot_gui.py
+cd ../qspectrumanalyzer/
 
-pause
+pyuic5 qspectrumanalyzer.ui -o ui_qspectrumanalyzer.py
+pyuic5 qspectrumanalyzer_baseline.ui -o ui_qspectrumanalyzer_baseline.py
+pyuic5 qspectrumanalyzer_colors.ui -o ui_qspectrumanalyzer_colors.py
+pyuic5 qspectrumanalyzer_persistence.ui -o ui_qspectrumanalyzer_persistence.py
+pyuic5 qspectrumanalyzer_settings_help.ui -o ui_qspectrumanalyzer_settings_help.py
+pyuic5 qspectrumanalyzer_settings.ui -o ui_qspectrumanalyzer_settings.py
+pyuic5 qspectrumanalyzer_smoothing.ui -o ui_qspectrumanalyzer_smoothing.py
